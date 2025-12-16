428b5aa5cb5823ac43d70cf434fc7e22a114a530
Amit Kumar Soni <eramitkumarsoni@users.noreply.github.com>
2025-12-16 12:47:48 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "FlutterSchedulerSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "FlutterSchedulerSDK",
+            targets: ["FlutterSchedulerSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "FlutterSchedulerSDK",
+            url: "https://github.com/eramitkumarsoni/flutter-scheduler-ios/releases/download/v1.0.0/FlutterSchedulerSDK.xcframework.zip",
+            checksum: "1f1b71e90b2dffdd253630253466da4a5cfa6ad894de19cf70ea1684508c3b15"
+        )
+    ]
+)

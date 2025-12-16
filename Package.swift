3668b7815866277eed421a0a74e651616885af42
Amit Kumar Soni <eramitkumarsoni@users.noreply.github.com>
2025-12-16 14:04:14 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FlutterSchedulerSDK",
-            url: "https://github.com/eramitkumarsoni/flutter-scheduler-ios/releases/download/v1.0.1/FlutterSchedulerSDK.xcframework.zip",
+            url: "https://github.com/eramitkumarsoni/flutter-scheduler-ios/releases/download/v1.0.0/FlutterSchedulerSDK.xcframework.zip",
             checksum: "1f1b71e90b2dffdd253630253466da4a5cfa6ad894de19cf70ea1684508c3b15"
         )
     ]
